75c4f0cf2a848ffb2231098152bd452bd93d4160
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-02 11:18:15 +0800

    Add animal model and the test of this

--- models/animal.js
+++ models/animal.js
@@ -1,8 +1,58 @@
 require('rootpath')()
 const Sequelize = require('sequelize')
 const db = require('util/db.js')
-const Animal = db.define('animal', {
+
+const animal = db.define('animal', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV1,
+    primaaryKey: true
+  },
   name: {
-    type: 
+    type: Sequelize.STRING
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  sex: {
+    type: Sequelize.STRING
+  },
+  build: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.STRING
+  },
+  chip_num: {
+    type: Sequelize.STRING
+  },
+  isSterilization: {
+    type: Sequelize.BOOLEAN
+  },
+  hair_type: {
+    type: Sequelize.STRING
+  },
+  children_anlong: {
+    type: Sequelize.BOOLEAN
+  },
+  animal_anlong: {
+    type: Sequelize.BOOLEAN
+  },
+  resettlement: {
+    type: Sequelize.STRING
+  },
+  note: {
+    type: Sequelize.TEXT("medium")
+  },
+  contact_phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  contact_email: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
 })
+
+module.exports = animal
---
+++ test/models/animal_test.js
@@ -0,0 +1,6 @@
+require('rootpath')()
+const animal = require('models/animal.js')
+const faker = require('faker/locale/zh_TW')
+
+
+
