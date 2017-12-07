70f3f6e614623ed93412c98d4f07c73e8c6f91dc
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-08 07:52:23 +0800

    fix the size of the list of animals

--- pages/animals/index.vue
+++ pages/animals/index.vue
@@ -1,24 +1,30 @@
 <template>
 <div>
-  <h1 class="text-center">
+  <h1 class="text-center banner">
    動物們
   </h1>
-  <table class="table">
-    <thead>
-      <th v-for="th in data['thead']"> {{ th }} </th>
-    </thead>
-    <tbody>
-      <tr v-for="td in data['tbody']" :key="td.id">
-        <td> {{ td.id }} </td>
-        <td> {{ td.name }} </td>
-        <td> {{ td.type }} </td>
-        <td> {{ td.sex }} </td>
-        <td> {{ td.resettlement }} </td>
-        <td> {{ td.contact_phone }} </td>
-        <td> {{ td.contact_email }} </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="row">
+    <div class="col"></div>
+    <div class="col">
+      <table class="table">
+        <thead>
+          <th v-for="th in data['thead']">  {{ th }} </th>
+        </thead>
+        <tbody>
+          <tr v-for="td in data['tbody']" :key="td.id">
+            <td> <a :href="'/animals/' + td.id"> {{ td.id }} </a> </td>
+            <td> {{ td.name }} </td>
+            <td> {{ td.type }} </td>
+            <td> {{ td.sex }} </td>
+            <td> {{ td.resettlement }} </td>
+            <td> {{ td.contact_phone }} </td>
+            <td> {{ td.contact_email }} </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col"></div>
+  </div>
 </div>
 </template>
 
