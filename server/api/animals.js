8f6c9b462571329a6aed27ce67489079041e44cd
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-08 07:53:06 +0800

    add api of find an animal by id

--- server/api/animals.js
+++ server/api/animals.js
@@ -16,12 +16,14 @@ router.get('/animals', function (req, res, next) {
 /* GET user by ID. */
 router.get('/animals/:id', function (req, res, next) {
   var id = req.params.id
+  var send_data = {}
+  send_data['thead'] = ['id', '名字', '種類', '性別', '體型', '年紀', '晶片編號', '是否絕育', '毛色', '是否可以跟小孩相處', '是否可以跟其他動物相處', '位置', '描述', '聯絡電話', '聯絡Email']
 
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
-  }
+  kb.find_animal(id).then(animal => {
+    send_data['tbody'] = animal
+    res.json(send_data)
+  }).catch(() => res.sendStatus(404))
+
 })
 
 export default router
