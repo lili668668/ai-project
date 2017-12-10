190d6dc7a15ebfbb2665ab944b569593f4b7ea07
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-10 10:19:33 +0800

    tmp

--- core/ie.js
+++ core/ie.js
@@ -4,10 +4,11 @@ const rx = require('rxjs/Rx')
 
 const is_fact = (fact) => {
   if (typeof fact === 'string') {
+    fact = fact.trim()
     var left_bracket_index = fact.indexOf('(')
     var right_bracket_index = fact.indexOf(')')
-    var describe = fact.substring(0, left_bracket_index)
-    var relations = fact.substring(left_bracket_index + 1, right_bracket_index)
+    var describe = fact.substring(0, left_bracket_index).trim()
+    var relations = fact.substring(left_bracket_index + 1, right_bracket_index).trim()
     if (describe === '' || relations === '') {
       return false
     } else {
@@ -18,4 +19,37 @@ const is_fact = (fact) => {
   }
 }
 
+const is_rule = (rule) => {
+  if (typeof rule === 'string') {
+    var sign_index = rule.indexOf(':-')
+    var left_fact = rule.substring(0, sign_index).trim()
+    var right_facts = rule.substring(sign_index + 2).trim().split(',')
+
+    for(var cnt = 0;cnt < right_facts.length;cnt++) {
+      if (!is_fact(right_facts[cnt])) {
+        return false
+      }
+    }
+
+    if (!is_fact(left_fact)) {
+      return false
+    }
+
+    return true
+    
+  } else {
+    return false
+  }
+}
+
+const inference = (facts, rules) => {
+  for(var cnt = 0;cnt < facts.length;cnt++) {
+    if (!is_fact(facts[cnt])) {
+      throw 'One of the fact is not correct.'
+    }
+  }
+}
+
 module.exports.is_fact = is_fact
+module.exports.is_rule = is_rule
+module.exports.inference = inference
--- test/core/ie_test.js
+++ test/core/ie_test.js
@@ -12,8 +12,49 @@ describe('inference engine', () => {
       assert.ok(!ie.is_fact('fruit()'))
     })
 
+    it('should return false when the string is not fact', () => {
+      assert.ok(!ie.is_fact('fruit'))
+    })
+
     it('should return false when the object is not string', () => {
       assert.ok(!ie.is_fact({}))
+    })
+  })
+
+  describe('#is_rule', () => {
+    it('should return true when the string is rule', () => {
+      assert.ok(ie.is_rule('fruit(x) :- juicy(x)'))
+    })
+
+    it('should return true when the string is rule', () => {
+      assert.ok(ie.is_rule('fruit(x) :- juicy(x), sweet(x)'))
+    })
+
+    it('should return false when the string is not rule. It is a fact.', () => {
+      assert.ok(!ie.is_rule('fruit(x)'))
+    })
+
+    it('should return false when the string is not rule. It is not format.', () => {
+      assert.ok(!ie.is_rule('fruit(x) :- '))
+    })
+
+    it('should return false when the string is not rule. Its fact is not format.', () => {
+      assert.ok(!ie.is_rule('fruit(x) :- joycy(x), sweet()'))
+    })
+
+    it('should return false when the object is not string.', () => {
+      assert.ok(!ie.is_rule([]))
+    })
+  })
+
+  describe('#inference', () => {
+    it('should throw error when all of the facts are not format.', () => {
+      try {
+        ie.inference(['fruit(apple)', 'apple'], ['fruit(x) :- juicy(x)', 'fruit(x) :- sweet(x)'])
+      } catch (err) {
+        should.exist(err).and.be.an.instanceOf(Error).with.property('message', 'One of the fact is not correct.')
+      }
+      assert.throws(ie.inference(['fruit(apple)', 'apple'], ['fruit(x) :- juicy(x)', 'fruit(x) :- sweet(x)']), Error, 'One of the fact is not correct.')
     })
   })
 })
