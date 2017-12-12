41c6fdafb03c6c41fe77b927e99ffe895a3ad28e
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-12 18:01:03 +0800

    tmp

---
+++ pages/facts/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1 class="text-center banner">
+   規則們
+  </h1>
+  <button class="mdc-fab material-icons" aria-label="Favorite">
+    <span class="mdc-fab__icon">
+      add
+    </span>
+  </button>
+</div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/animals')
+    return { data: data }
+  }
+}
+</script>
+
+<style>
+</style>
