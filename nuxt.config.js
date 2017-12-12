1fd6cee749454aba6d45c236b188e5aa42407b32
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-12 18:00:50 +0800

    Do some change

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,23 +11,29 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: "stylesheet", type: "text/css", href: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" }
+      { rel: "stylesheet", type: "text/css", href: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" },
+      { rel: "stylesheet", type: "text/css", href: "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" }
     ],
     script: [
       { src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js", type: 'text/javascript' },
       { src: "https://code.jquery.com/jquery-3.2.1.slim.min.js", type: 'text/javascript' },
       { src: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js", type: 'text/javascript' },
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js", type: 'text/javascript' }
     ]
   },
   /*
   ** Global CSS
   */
   css: ['~/assets/css/main.css'],
+  env: {
+    nuxt_HOST: process.env.HOST || '127.0.0.1',
+    nuxt_PORT: process.env.PORT || 3000
+  },
   /*
   ** Add axios globally
   */
   build: {
-    vendor: ['axios'],
+    vendor: ['axios', 'vue-socket.io', 'socket.io-client'],
     /*
     ** Run ESLINT on save
     */
@@ -41,5 +47,6 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  plugins: [ { src: '~/plugins/socket.io', ssr: false } ]
 }
